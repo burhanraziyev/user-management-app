e7bc1904c7350a20e44c0485afab8e4d1fda4d8d
Burhan Raziyev <burkhanraziyev1@gmail.com>
2023-01-02 08:40:00 +0400

    initial commit

---
+++ src/main/java/az/burhan/user/management/app/domain/User.java
@@ -0,0 +1,64 @@
+package az.burhan.user.management.app.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@DynamicInsert
+@DynamicUpdate
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @SequenceGenerator(name = "user_id_seq", sequenceName = "user_id_seq")
+    @GeneratedValue(strategy = SEQUENCE, generator = "user_id_seq")
+    @Column(updatable = false, insertable = false, nullable = false)
+    private Long id;
+
+
+    @CreatedDate
+    @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(nullable = false, insertable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @CreatedBy
+    @Column(nullable = false, insertable = false, updatable = false)
+    private String createdBy;
+
+    @LastModifiedDate
+    @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(nullable = false, insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+    @LastModifiedBy
+    @Column(nullable = false, insertable = false)
+    private String lastModifiedBy;
+}
---
+++ src/main/java/az/burhan/user/management/app/mapper/PermissionMapper.java
@@ -0,0 +1,7 @@
+package az.burhan.user.management.app.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface PermissionMapper {
+}
